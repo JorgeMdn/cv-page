4c7d46d03631552babfefbab4e36b534fd3d0861
Jorge Medina <jlsilva@ectotec.com>
2022-10-07 21:07:24 -0500

    feat(): Advance `navbar`

--- js/events.js
+++ js/events.js
@@ -1,29 +1,15 @@
-//Record how many pixels are scrolled vertically
-let scrollpos = window.scrollY;
-
-var header = document.getElementById("navbar");
-console.log(header);
-//How many pixels you need to scroll to add a class. You can change the value
+const header = document.getElementById("navbar");
+const body = document.getElementById("body");
 const scrollChange = 1;
 
-//The function that will add the class
 const add_class_on_scroll = () => header.classList.add("nav--scrolled");
 const remove_class_on_scroll = () => header.classList.remove("nav--scrolled");
 
-//Tracking a "scroll" event
-window.addEventListener("scroll", function () {
-  scrollpos = window.scrollY;
-  console.log(scrollpos);
-  //If we scrolled more than we specified in the scrollChange variable, the add class function is executed
-  if (scrollpos >= scrollChange) {
+body.addEventListener("scroll", function (e) {
+  const scrollPos = e.target.scrollTop;
+  if (scrollPos >= scrollChange) {
     add_class_on_scroll();
   } else {
     remove_class_on_scroll();
   }
 });
-
-/* 
-$(window).scroll(function (event) {
-  var scroll = $(window).scrollTop();
-  // Do something
-}); */
