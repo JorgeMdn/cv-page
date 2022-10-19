d854bfb1208db64a6b4c04b8bb95d8b6564440ce
Jorge Medina <jorge.si.med@gmail.com>
2022-10-19 08:47:46 -0500

    feat(): Advance contact me and experience

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// window.location.replace("http://127.0.0.1:5500/#skill");
+
+// window.location.href = "http://127.0.0.1:5500/#skill";
+
+const element = document.getElementById("contact_me");
+element.scrollIntoView();
---
+++ js/rubber_band.js
@@ -0,0 +1,18 @@
+const blastItemList = document.querySelectorAll(".blast__item");
+const bannerTitle = document.querySelector(".banner__title");
+console.log(bannerTitle);
+blastItemList.forEach((blast) => {
+  blast.addEventListener("mouseenter", () => {
+    blast.classList.add("blast__item--animation");
+    setTimeout(() => {
+      blast.classList.remove("blast__item--animation");
+    }, 500);
+  });
+});
+
+bannerTitle.addEventListener("mouseenter", () => {
+  bannerTitle.classList.add("banner__title--animation");
+  setTimeout(() => {
+    bannerTitle.classList.remove("banner__title--animation");
+  }, 1000);
+});
